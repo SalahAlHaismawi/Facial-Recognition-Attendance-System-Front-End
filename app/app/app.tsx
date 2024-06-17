f5bca0c07f00bc1697886b5e1872a5ec30a7ea35
Salah AH <77612931+SalahAlHaismawi@users.noreply.github.com>
2024-06-18 03:20:50 +0800

    t

--- app/app/app.tsx
+++ app/app/app.tsx
@@ -1,12 +1,16 @@
 import type { AppProps } from 'next/app';
 import {app, auth} from '../firebaseConfig'
 import { AuthProvider } from '../context/AuthContext';
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 function MyApp({ Component, pageProps }: AppProps) {
   console.log('app');
   return (
     <AuthProvider>
-      <Component {...pageProps} />
+        <SpeedInsights>
+            <Component {...pageProps} />
+
+        </SpeedInsights>
     </AuthProvider>
   );
 }
